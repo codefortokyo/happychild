c76829e5ac8a216a1d312bcbdcced3c5eca47a17
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-10-07 16:56:45 +0900

    Add service information page

---
+++ views/misc_views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+from infrastructure.mysql import Ward
+
+
+def about(request: HttpRequest) -> render:
+    return render(request, 'about.html', context={
+        'wards': Ward.objects.filter(is_active=True)
+    })
