4387a1562a5eaa3ff8ee15a8937ba75201a92492
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-10-13 16:27:22 +0900

    Add form of editing nursery free num

--- views/admin_views.py
+++ views/admin_views.py
@@ -2,8 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 
-from infrastructure.models import Nursery, Ward
-from services.forms.admins import NurseryForm
+from infrastructure.models import Nursery, NurseryFreeNum, Ward
+from infrastructure.consts import NURSERY_INFO, NURSERY_FREE_NUM, NURSERY_SCORE
+from services.forms.admins import NurseryForm, NurseryFreeNumForm
+from services.transformers import transform_free_num_form_to_free_num
 
 
 @login_required
@@ -20,15 +22,28 @@ def nursery(request: HttpRequest, ward_id: int, nursery_id: int) -> render or re
         return render(request, 'admin/nursery.html', context={
             'ward': Ward.objects.filter(id=ward_id).first(),
             'nursery_id': nursery_id,
-            'form': NurseryForm(instance=Nursery.objects.get(pk=nursery_id))
+            'form': NurseryForm(instance=Nursery.objects.get(pk=nursery_id)),
+            'form_free_num': NurseryFreeNumForm()
         })
-    form = NurseryForm(request.POST, instance=Nursery.objects.get(pk=nursery_id))
-    if form.is_valid():
-        form.save()
-        return redirect('/admin/wards/{}/nurseries/'.format(ward_id))
+
+    form_type = request.POST.get('form_type')
+
+    if form_type == NURSERY_INFO:
+        form = NurseryForm(request.POST, instance=Nursery.objects.get(pk=nursery_id))
+        if form.is_valid():
+            form.save()
+            return redirect('/admin/wards/{}/nurseries/'.format(ward_id))
+
+    elif form_type == NURSERY_FREE_NUM:
+        form = NurseryFreeNumForm(request.POST)
+        if form.is_valid():
+            entities = transform_free_num_form_to_free_num(form, nursery_id)
+            NurseryFreeNum.bulk_insert(entities)
+            return redirect('/admin/wards/{}/nurseries/'.format(ward_id))
+
     return render(request, 'admin/nursery.html', context={
         'ward': Ward.objects.filter(id=ward_id).first(),
         'nursery_id': nursery_id,
-        'form': form
+        'form': NurseryForm(request.POST, instance=Nursery.objects.get(pk=nursery_id)),
+        'form_free_num': NurseryForm(request.POST),
     })
-
