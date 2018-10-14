6ba958f6e4da8bc3bedef35c219a7cd9d4450332
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-10-14 11:25:56 +0900

    Update run commands for docker-compose

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
+python manage.py migrate
 
 exec gunicorn happy_child.wsgi:application -b 0.0.0.0:8000 -w 4
