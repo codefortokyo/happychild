95547e4f0f1fb01b4a6bbe0861787817e9640e33
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-10-29 00:24:38 +0900

    Set default as Shinagawa

--- views/top_views.py
+++ views/top_views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+from infrastructure.models import Ward
 from services.forms.searches import SearchLocationForm, SearchTypeForm, SearchFeatureForm
+
+DEFAULT_WARD_ID = 9
 
 
 def index(request: HttpRequest) -> render:
+    ward = Ward.objects.filter(id=DEFAULT_WARD_ID).first()
     return render(request, 'index.html', context={
-        'location_form': SearchLocationForm(),
+        'location_form': SearchLocationForm(initial={
+            'city': ward.city_id,
+            'ward': ward.id
+        }, city_id=ward.city_id, ward_id=ward.id),
         'type_form': SearchTypeForm(),
         'feature_form': SearchFeatureForm(),
     })
+
