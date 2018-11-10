39803ad0d34b7ef3834e6de079012449bf8e9e8d
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-11-10 20:43:08 +0900

    Do makemigrations before migrate

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 
 exec gunicorn happychild.wsgi:application -b 0.0.0.0:8000 -w 4
