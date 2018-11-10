999e451042a946d71a20325a51ebf99564a9e406
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-11-10 19:02:27 +0900

    s/happy-child/happychild

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ python manage.py collectstatic --clear --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-exec gunicorn happy_child.wsgi:application -b 0.0.0.0:8000 -w 4
+exec gunicorn happychild.wsgi:application -b 0.0.0.0:8000 -w 4
