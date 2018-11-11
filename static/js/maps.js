d1fbc68e4c401f5622c1038ab826ca4ee57f35f5
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-11-11 15:46:29 +0900

    Set detail page link

--- static/js/maps.js
+++ static/js/maps.js
@@ -96,7 +96,7 @@ function drawInfoWindow(property) {
         "<img src='" + image + "'/>" +
         "</div>" +
         "<div class='map-content'>" +
-        "<h4><a href='#'>" + name + "</a></h4>" +
+        "<h4><a href='/nursery/" + id + "'>" + name + "</a></h4>" +
         "<p class='address'> <i class='fa fa-map-marker'></i>" + address + "</p>" +
         "<p class='description'>最寄り " + station_info + "</p>" +
         "<div class='map-properties-fetures'> " +
@@ -222,7 +222,7 @@ function insertPropertyToArray(property, layout) {
     if (layout == 'grid_layout') {
         element = '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 js-matchHeight"><div class="property">' +
             '<!-- Property img --> ' +
-            '<a href="/nursery/' + id + '" class="property-img" target="_blank">' +
+            '<a href="/nursery/' + id + '" class="property-img">' +
             '<div class="property-tag button sale">' + image + '</div> ' +
             '<img src="' + image + '" alt="properties-3" class="img-responsive"> ' +
             '</a>' +
@@ -230,7 +230,7 @@ function insertPropertyToArray(property, layout) {
             '<div class="property-content"> ' +
             '<!-- title --> ' +
             '<h1 class="title">' +
-            '<a href="/nursery/' + id + '" target="_blank">' + name + '</a> ' +
+            '<a href="/nursery/' + id + '">' + name + '</a> ' +
             '</h1> ' +
             '<!-- Property address --> ' +
             '<h3 class="property-address"> ' +
@@ -269,7 +269,7 @@ function insertPropertyToArray(property, layout) {
         element = '' +
             '<div class="property map-properties-list clearfix"> ' +
             '<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 js-matchHeight"> ' +
-            '<a href="properties-details.html" class="property-img height"> ' +
+            '<a href="/nursery/' + id + '" class="property-img height">' +
             '<div class="property-tag button sale">' + image + '</div> ' +
             '<img src="' + image + '" alt="properties" class="img-responsive img-inside-map"> ' +
             '</a> ' +
@@ -277,7 +277,7 @@ function insertPropertyToArray(property, layout) {
             '<div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 property-content "> ' +
             '<!-- title --> ' +
             '<h1 class="title"> ' +
-            '<a href="">' + name + '</a> </h1> ' +
+            '<a href="/nursery/"' + id + '">' + name + '</a> </h1> ' +
             '<!-- Property address --> ' +
             '<h3 class="property-address"> ' +
             '<a href=""> ' +
