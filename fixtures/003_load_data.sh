35677c41f9bec9e4be595a832d16b76cf4cc0bf8
ogiogi93 <takashi-ogiwara@haveagood.holiday>
2018-11-11 13:29:56 +0900

    s/happy-child/happychild

--- fixtures/003_load_data.sh
+++ fixtures/003_load_data.sh
@@ -4,6 +4,6 @@ cd /docker-entrypoint-initdb.d/
 
 # import data
 tar xzvf dataset.tar.gz
-mysql -h localhost -u root happy_child -e "LOAD DATA LOCAL INFILE 'dataset/nurseries.csv' INTO TABLE nurseries  FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES"
-mysql -h localhost -u root happy_child -e "LOAD DATA LOCAL INFILE 'dataset/nursery_free_nums.csv' INTO TABLE nursery_free_nums  FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES"
-mysql -h localhost -u root happy_child -e "LOAD DATA LOCAL INFILE 'dataset/stations.csv' INTO TABLE stations  FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql -h localhost -u root happychild -e "LOAD DATA LOCAL INFILE 'dataset/nurseries.csv' INTO TABLE nurseries  FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql -h localhost -u root happychild -e "LOAD DATA LOCAL INFILE 'dataset/nursery_free_nums.csv' INTO TABLE nursery_free_nums  FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES"
+mysql -h localhost -u root happychild -e "LOAD DATA LOCAL INFILE 'dataset/stations.csv' INTO TABLE stations  FIELDS TERMINATED BY ',' ENCLOSED BY '\"' IGNORE 1 LINES"
